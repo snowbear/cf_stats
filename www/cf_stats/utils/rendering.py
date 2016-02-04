d3fb0e570c38d6f87eac6e5224e60f2d030cb5fe
Marat Yuldashev <marat.snowbear@gmail.com>
2016-02-04 03:06:51 +0200

    first version of render() method

--- www/cf_stats/utils/rendering.py
+++ www/cf_stats/utils/rendering.py
@@ -1,36 +1,68 @@
-class Br:
-    def __repr__(self, *args, **kwargs):
-        return "Br"
+import datetime
 
 
-class List:
+def format_time(timedelta):
+    s = int(timedelta.total_seconds())
+    h = s // 60 // 60
+    m = s // 60 % 60
+    return "({h:02}:{m:02})".format(h=h, m=m)
+
+
+class RenderingItemBase:
+    def render(self):
+        raise NotImplementedError()
+
+
+class Br(RenderingItemBase):
+    def render(self):
+        return "<br />"
+
+
+class List(RenderingItemBase):
     def __init__(self, separator, *items):
+        assert isinstance(separator, (str, RenderingItemBase))
+
         self.separator = separator
         self.items = items
 
-    def __repr__(self):
-        return "List({separator}, {items})".format(separator=self.separator.__repr__(),
-                                                   items=','.join(i.__repr__() for i in self.items))
+    def render(self):
+        separator = self.separator if isinstance(self.separator, str) else self.separator.render()
+        return separator.join(i.render() for i in self.items)
 
 
-class Text:
-    def __init__(self, text):
+class Text(RenderingItemBase):
+    def __init__(self, text, bold=False):
         self.text = text
+        self.bold = bold
 
-    def __repr__(self, *args, **kwargs):
-        return "Text({text})".format(text=self.text.__repr__())
+    def render(self):
+        result = self.text
+        if self.bold:
+            result = "<b>{0}</b>".format(result)
+        return result
 
 
-class Party:
+class Party(RenderingItemBase):
     def __init__(self, party):
         self.party = party
 
-    def __repr__(self, *args, **kwargs):
-        return "[{handle}]".format(handle=self.party.members[0].handle)
+    def render(self):
+        return "<a>{name}<a>".format(name=self.party.party_name)
 
 
-class FirstAcceptedStatRow:
+class FirstAcceptedStatRow(RenderingItemBase):
     def __init__(self, problem, relative_time, parties):
         self.problem = problem
         self.relative_time = relative_time
         self.parties = parties
+
+    def render(self):
+        problem_caption = Text(self.problem.index, bold=True)
+        if self.relative_time is None:
+            return problem_caption.render()
+        else:
+            formatted_time = format_time(datetime.timedelta(minutes=self.relative_time))
+            return List(' - ',
+                        problem_caption,
+                        Text(formatted_time),
+                        List(' ', *[Party(p) for p in self.parties])).render()
